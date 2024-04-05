8f585749ede4db73a7bd7462ac470ef30d94a8a5
Aline Silveira <aline@outlook.ie>
2024-04-04 22:20:16 -0300

    atualizado

--- app/ETL/extract.py
+++ app/ETL/extract.py
@@ -1,28 +1,15 @@
-import os #biblioteca para manipular arquivos e pastas
-import glob #biblioteca para listar arquivos
+import glob
+import os
 import pandas as pd
-from typing import List
 
-"""
-Função para ler os arquivos de uma pasta
-data/input e retornar uma lista de dataframes
+def extract_excel(input_folder):
+    """
+    função para extrair dados de arquivos Excel.
 
-args: input_path (str): caminho da parta com os arquivos
-
-return: lista de dataframes
-
-"""
-path = "data/input"
-
-def extract_from_excel(path: str) -> List[pd.DataFrame]:
-    all_files = glob.glob(os.path.join(path, "*.xlsx"))
-
-    data_frame_list = []
-    for file in all_files:
-        data_frame_list.append(pd.read_excel(file))
-
-    return data_frame_list
-
-if __name__ == '__main__':
-    data_frame_list = extract_from_excel(path)
-    print(data_frame_list)
+    type: input_folder: str
+    """
+    files = glob.glob(os.path.join(input_folder, "*.xlsx"))
+    if not files:
+        raise ValueError("No Excel files found in the specified folder")
+    all_data = [pd.read_excel(file) for file in files]
+    return all_data
